4995411dd67fed7ae8dea4d56f457d8434cab964
weikton <weikton@vk.com>
2026-03-08 18:25:06 +0200

    Reversed `CTaskComplexGoToPointShooting`

--- source/game_sa/Tasks/TaskTypes/TaskComplexGoToPointShooting.cpp
+++ source/game_sa/Tasks/TaskTypes/TaskComplexGoToPointShooting.cpp
@@ -8,8 +8,8 @@ void CTaskComplexGoToPointShooting::InjectHooks() {
 
     RH_ScopedInstall(Constructor, 0x668C70);
 
-    RH_ScopedVMTInstall(Clone, 0x66CE10, { .reversed = false });
-    RH_ScopedVMTInstall(GetTaskType, 0x668CC0, { .reversed = false });
+    RH_ScopedVMTInstall(Clone, 0x66CE10);
+    RH_ScopedVMTInstall(GetTaskType, 0x668CC0);
 }
 
 // 0x668C70
